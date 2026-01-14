ea556c35f407d3c52d100cab967f4c3bcf7ff96a
juraev <muhammadjonjuraev18@gmail.com>
2026-01-14 18:10:35 +0500

    .

---
+++ Infrastructure/Interface/IStudentService.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Dto.Student;
+using Domain.Responses;
+
+namespace Infrastructure.Interface;
+
+public interface IStudentService
+{
+    public Response<string> RegisterStudent(CreatedStudentDto dto);
+    public Response<string> UpdateStudent(UpdateStudentDto dto);
+    public Response<string> DeleteStudent(int id);
+    public Response<GetStudentDto> GetStudentById(int id);
+    public Response<GetStudentDto> GetAllStudentWithCard(int cardId);
+    public Response<List<GetStudentDto>> GetAllStudentsWithCards();
+    public Response<List<GetStudentDto>> GetStudentsByCourse(int courseId);
+    public Response<GetStudentDto> GetStudentByCardNumber(string cardNumber);
+}
